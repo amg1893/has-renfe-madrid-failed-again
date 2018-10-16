39ddcf9c08b48255e259ea2d50e85d5b588f0991
Adrián Mora <amora@adrianmora.me>
2018-10-16 11:04:58 +0200

    what about now

--- web/index.php
+++ web/index.php
@@ -43,7 +43,7 @@ $app->get('/', function (\Symfony\Component\HttpFoundation\Request $request) use
     $app['monolog']->addDebug('logging output.');
     $hashtags = $app['db']->fetchAll('SELECT * FROM hashtag_status');
     $ret = ['hashtags' => $hashtags];
-    if ($request->getAcceptableContentTypes() === 'application/json') {
+    if (count($request->getAcceptableContentTypes()) === 1 && $request->getAcceptableContentTypes()[0] === 'application/json') {
         return $app->json($ret);
     }
     return $app['twig']->render('index.twig', $ret);
