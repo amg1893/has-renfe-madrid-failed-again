940d1abb390cab97a84189a585d31baf255c764b
Adrián Mora <amora@adrianmora.me>
2018-10-16 13:34:07 +0200

    migrations for database

---
+++ src/Entity/Latest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\LatestRepository")
+ */
+class Latest
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="bigint")
+     */
+    private $last_id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $date_tweet;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLastId(): ?int
+    {
+        return $this->last_id;
+    }
+
+    public function setLastId(int $last_id): self
+    {
+        $this->last_id = $last_id;
+
+        return $this;
+    }
+
+    public function getDateTweet(): ?string
+    {
+        return $this->date_tweet;
+    }
+
+    public function setDateTweet(string $date_tweet): self
+    {
+        $this->date_tweet = $date_tweet;
+
+        return $this;
+    }
+}
