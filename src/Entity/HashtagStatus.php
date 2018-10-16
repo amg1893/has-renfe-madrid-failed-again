d8c207165e3908d4ba689da4b4dbc0ec2b1d9874
Adrián Mora <amora@adrianmora.me>
2018-10-16 17:17:20 +0200

    added abstract entity to have some common methods

--- src/Entity/HashtagStatus.php
+++ src/Entity/HashtagStatus.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\HashtagStatusRepository")
  */
-class HashtagStatus
+class HashtagStatus extends AbstractEntity
 {
     /**
      * @ORM\Id()
--- src/Entity/Latest.php
+++ src/Entity/Latest.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\LatestRepository")
  */
-class Latest
+class Latest extends AbstractEntity
 {
     /**
      * @ORM\Id()
