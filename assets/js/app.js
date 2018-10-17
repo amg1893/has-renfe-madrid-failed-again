9f639966aba792462e839c33e8715a34c7d65e19
Adrián Mora <amora@adrianmora.me>
2018-10-17 13:33:57 +0200

    added purecss and another try to add libraries from node_modules

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,10 +5,16 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+var purecss = require('purecss');
+require(purecss.getFile('pure-min'));
+require(purecss.getFile('grids-responsive-min'));
+
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // var $ = require('jquery');
 
+var axios = require('axios');
+var twttr = require('twitter-widgets');
 
