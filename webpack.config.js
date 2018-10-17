0f8e3279ec0297586727e408e5a973c784f5e506
Adrián Mora <adrian.mora@webfg.com>
2018-10-17 10:38:37 +0200

    i give up on axios and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -18,9 +18,6 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
-    .addPlugin(new webpack.ProvidePlugin({
-        axios: 'axios'
-    }))
     .addEntry('app', './assets/js/app.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
