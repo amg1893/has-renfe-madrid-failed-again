b1411a36fad610c072aa102eacee7a3bcb64a6cd
Adrián Mora <adrian.mora@webfg.com>
2018-10-17 16:31:54 +0200

    typo

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,7 @@
  */
 
 require('purecss');
-require('font-awesome');
+require('font-awesome-webpack');
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
