d8d462c0da679d353e7aa335162b227b1b1da37d
Adrián Mora <adrian.mora@webfg.com>
2018-10-18 15:57:18 +0200

    log exceptions

--- src/EventListener/ErrorExceptionListener.php
+++ src/EventListener/ErrorExceptionListener.php
@@ -2,16 +2,26 @@
 
 namespace App\EventListener;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class ErrorExceptionListener
 {
+    /** @var \Psr\Log\LoggerInterface $logger */
+    protected $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
         $exception = $event->getException();
+
+        $this->logger->error((string) $exception);
 
         $response = new JsonResponse([
             'result' => false,
@@ -22,7 +32,7 @@ class ErrorExceptionListener
             $response->setStatusCode($exception->getStatusCode());
             $response->headers->replace($exception->getHeaders());
         } else {
-            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+            $response->setStatusCode(JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
         }
 
         // sends the modified response object to the event
