7f7011305242b3709f8aacc1a4e2a973ce19c4c3
Adrián Mora <adrian.mora@webfg.com>
2018-10-24 15:18:09 +0200

    offline cache

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,7 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-require('offline-plugin/runtime').install();
+//require('offline-plugin/runtime').install();
 
 require('purecss');
 require('purecss/build/grids-responsive-min.css');
@@ -29,3 +29,17 @@ TwitterWidgetsLoader.load(function(){
   console.log('Twitter loaded');
   document.init();
 });
+
+//This is the service worker with the Cache-first network
+
+//Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
+if (navigator.serviceWorker.controller) {
+  console.log('[PWA Builder] active service worker found, no need to register')
+} else {
+//Register the ServiceWorker
+  navigator.serviceWorker.register('sw.js', {
+    scope: './'
+  }).then(function(reg) {
+    console.log('Service worker has been registered for scope:'+ reg.scope);
+  });
+}
