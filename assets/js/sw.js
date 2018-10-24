0fa3bd38be6ee7e2836ae43524e2b6a3d919f21c
Adrián Mora <amora@adrianmora.me>
2018-10-24 16:51:20 +0200

    different caching method

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,7 +1,19 @@
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-        })
-    );
+var cacheName = 'HRMFA';
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.method === 'GET') {
+        event.respondWith(
+            caches.match(event.request).then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).then((response) => {
+                    return caches.open(cacheName).then((cache) => {
+                        cache.put(event.request.url, response.clone());
+                        return response;
+                    });
+                });
+            })
+        );
+    }
 });
