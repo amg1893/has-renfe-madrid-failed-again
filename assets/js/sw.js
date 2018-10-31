1c0a16731afb9bd58b33493dbe6bdef8396161c2
Adrián Mora <amora@adrianmora.me>
2018-10-31 16:30:25 +0100

    changing code

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -15,23 +15,3 @@ self.addEventListener('fetch', function(event) {
     );
   }
 });
-
-let deferredPrompt;
-let installButton = document.getElementById('installButton');
-
-self.addEventListener('beforeinstallprompt', (e) => {
-    e.preventDefault();
-    deferredPrompt = e;
-
-    installButton.style.display = 'inline-block;';
-});
-
-installButton.addEventListener('click', (e) => {
-  deferredPrompt.prompt();
-  deferredPrompt.userChoice
-    .then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        installButton.style.display = 'none';
-      }
-    });
-});
