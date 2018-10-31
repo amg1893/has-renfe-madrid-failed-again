8203e1a039fa3945f873684b94554bcd7dcda628
Adrián Mora <amora@adrianmora.me>
2018-11-01 00:03:30 +0100

    added version number to the bottom of the page

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -4,16 +4,20 @@ namespace App\Controller;
 
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as SymfonyAbstractController;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 abstract class AbstractController extends SymfonyAbstractController
 {
     /** @var \Psr\Log\LoggerInterface $logger */
-    protected $logger = null;
+    protected $logger;
     /** @var \Symfony\Component\HttpFoundation\Request $request */
-    protected $request = null;
+    protected $request;
+    /** @var KernelInterface $kernel */
+    protected $kernel;
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, KernelInterface $kernel)
     {
+        $this->kernel = $kernel;
         $this->logger = $logger;
         $this->request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
     }
